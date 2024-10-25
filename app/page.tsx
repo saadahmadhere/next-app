e7cedbb884b8b65dac6541833f7edc4be110c49d
saadahmadhere <saadahmad728@gmail.com>
2024-10-26 00:32:28 +0530

    feat: prettify UI

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,17 @@ async function getUserDetails() {
 }
 
 export default async function Home() {
-	const userDetails = await getUserDetails();
+	const userData = await getUserDetails();
+
 	return (
-		<div>
-			<div>{userDetails.name}</div>
-			<div>{userDetails.email}</div>
+		<div className='flex flex-col justify-center h-screen'>
+			<div className='flex justify-center'>
+				<div className='border p-8 rounded'>
+					<div>Name: {userData?.name}</div>
+
+					{userData?.email}
+				</div>
+			</div>
 		</div>
 	);
 }
