76a32d9fd1d4adcec2641c73f9a081efcb259e17
saadahmadhere <saadahmad728@gmail.com>
2024-10-26 00:30:28 +0530

    feat: add api call on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-	return <div>hello world</div>;
+import axios from 'axios';
+
+async function getUserDetails() {
+	const response = await axios.get(
+		'https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details'
+	);
+	return response.data;
+}
+
+export default async function Home() {
+	const userDetails = await getUserDetails();
+	return (
+		<div>
+			<div>{userDetails.name}</div>
+			<div>{userDetails.email}</div>
+		</div>
+	);
 }
