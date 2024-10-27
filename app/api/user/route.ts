30e7de746278f81f82b371e3393f45e3bfd02549
saadahmadhere <saadahmad728@gmail.com>
2024-10-27 23:28:53 +0530

    feat: add POST signup route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,3 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 export async function GET() {
 	return Response.json({ username: 'harkirat', email: 'harkirat@gmail.com' });
+}
+
+export async function POST(req: NextRequest) {
+	const body = await req.json();
+
+	return NextResponse.json({
+		username: body.username,
+		password: body.password,
+	});
 }
