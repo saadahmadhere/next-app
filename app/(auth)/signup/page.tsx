b06115149a5a7b43f3577a38d45aaf99cf8ceb58
saadahmadhere <saadahmad728@gmail.com>
2024-10-29 07:44:33 +0530

    fix: variable names

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -7,7 +7,7 @@ export default function Signup() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 
-	const handleSignInClick = async () => {
+	const handleSignUpClick = async () => {
 		const response = await axios.post('http://localhost:3000/api/user', {
 			username,
 			password,
@@ -44,7 +44,7 @@ export default function Signup() {
 								placeholder='123456'
 							/>
 							<button
-								onClick={handleSignInClick}
+								onClick={handleSignUpClick}
 								type='button'
 								className='mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
 							>
