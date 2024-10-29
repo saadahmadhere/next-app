6c34689298cc550447eb84a5fe77992be79c7a1e
saadahmadhere <saadahmad728@gmail.com>
2024-10-29 08:03:59 +0530

    feat: add signup post route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
 
 export async function GET() {
 	return Response.json({ username: 'harkirat', email: 'harkirat@gmail.com' });
@@ -7,8 +10,43 @@ export async function GET() {
 export async function POST(req: NextRequest) {
 	const body = await req.json();
 
-	return NextResponse.json({
-		username: body.username,
-		password: body.password,
-	});
+	try {
+		// Check if the user already exists
+		const existingUser = await client.user.findUnique({
+			where: {
+				username: body.username,
+			},
+		});
+
+		if (existingUser) {
+			return NextResponse.json(
+				{
+					message: 'User already exists',
+				},
+				{ status: 409 }
+			); // 409 Conflict
+		}
+
+		// Create new user
+		const user = await client.user.create({
+			data: {
+				username: body.username,
+				password: body.password,
+			},
+		});
+
+		console.log('from BE', { user });
+
+		return NextResponse.json({
+			message: 'User signed up',
+		});
+	} catch (error) {
+		console.log('error, ', error);
+		return NextResponse.json(
+			{
+				message: 'An error occurred',
+			},
+			{ status: 500 }
+		); // 500 Internal Server Error
+	}
 }
